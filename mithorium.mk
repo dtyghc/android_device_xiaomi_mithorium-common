154019d45e8c03fe0e7f60580ca41d72388073d1
Abhi <cocabhi8020@gmail.com>
2022-08-27 15:34:32 +0000

    Mi439: Bringup CorvusOS
    Signed-off-by: Abhi <cocabhi8020@gmail.com>

--- mithorium.mk
+++ mithorium.mk
@@ -19,7 +19,7 @@ endif
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
-DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-lineage
+DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-corvus
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += $(LOCAL_PATH)/overlay/packages/apps/CarrierConfig
